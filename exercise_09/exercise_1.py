c4af2cde0e1b310b76d074aeb881c4211ed1718a
Sagar Kishore <47300709+sagarkishore-7@users.noreply.github.com>
2023-02-17 23:40:36 +0100

    Added exercise_09

--- exercise_09/exercise_1.py
+++ exercise_09/exercise_1.py
@@ -27,7 +27,15 @@ def search_correct(x: int, seq: List[int]) -> int:
 
 ### Test suite ###
 TESTCASES: List[Tuple[Tuple[int, List[int]], int]] = [
-    # TODO: YOUR TESTCASES HERE
+    ((4, [1, 2, 3]), 3),
+    ((4, [1, 2, 3, 4]), 3),
+    ((4, [4, 3, 2, 1]), 0),
+    ((4, [3, 4, 2, 1]), 1),
+    ((4, [3, 2, 4, 1]), 2),
+    ((4, [4, 4, 4, 4]), 0),
+    ((5, [4, 3, 2, 1]), 4),
+    ((5, [4, 3, 2, 5]), 3),
+    ((2, [3, 2, 4, 1]), 0)
 ]
 
 ### Repair ###
